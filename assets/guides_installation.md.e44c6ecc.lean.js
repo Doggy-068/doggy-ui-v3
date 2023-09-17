3fde3b3a0fc1582b64b6a7623d7964af8c21151e
Doggy-068 <728724654@qq.com>
2023-09-17 09:50:04 +0800

    Updates

---
+++ assets/guides_installation.md.e44c6ecc.lean.js
@@ -0,0 +1,5 @@
+import{_ as a,o as e,c as t,S as o,k as l}from"./chunks/framework.41ef2244.js";const f=JSON.parse('{"title":"Installation","description":"","frontmatter":{},"headers":[],"relativePath":"guides/installation.md","filePath":"guides/installation.md"}'),n={name:"guides/installation.md"},s=o("",4),i=l("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"Hello World",src:"https://codepen.io/Doggy068/embed/WNLZrMP?default-tab=html%2Cresult&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/Doggy068/pen/WNLZrMP">
+  Hello World</a> by Doggy (<a href="https://codepen.io/Doggy068">@Doggy068</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),r=[s,i];function d(c,h,g,p,_,u){return e(),t("div",null,r)}const y=a(n,[["render",d]]);export{f as __pageData,y as default};
